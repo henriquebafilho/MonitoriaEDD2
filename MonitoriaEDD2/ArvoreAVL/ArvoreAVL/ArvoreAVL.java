b6b32c8aff968170c7bfe694ef8056c238cf070b
Henrique Filho <hbaf71@gmail.com>
2020-04-17 18:55:35 -0300

    Função de mostrar árvore

--- MonitoriaEDD2/ArvoreAVL/ArvoreAVL/ArvoreAVL.java
+++ MonitoriaEDD2/ArvoreAVL/ArvoreAVL/ArvoreAVL.java
@@ -4,52 +4,45 @@ package ArvoreAVL;
 
 public class ArvoreAVL<T extends Comparable<T>> {
 
-	//Todo nó de uma árvore AVL é uma árvore, contendo uma chave raiz com um valor, outra árvore a esquerda e outra a direita, que podem ser nulas ou não
-	private T raiz; 
-	private ArvoreAVL esq, dir; 
-	
-	/*public class No {
-		private T raiz; 
-		private No esq, dir; 
+	// Todo nó de uma árvore AVL é uma árvore, contendo uma chave raiz com um valor,
+	// outra árvore a esquerda e outra a direita, que podem ser nulas ou não
+	private T raiz;
+	private ArvoreAVL esq, dir;
 
-		public No(T raiz) {
-			this.raiz = raiz;
-			this.esq = null;
-			this.dir = null;
-		}
+	/*
+	 * public class No { private T raiz; private No esq, dir;
+	 * 
+	 * public No(T raiz) { this.raiz = raiz; this.esq = null; this.dir = null; }
+	 * 
+	 * public No(T raiz, No esq, No dir) { this.raiz = raiz; this.esq = esq;
+	 * this.dir = dir; } }
+	 */
 
-		public No(T raiz, No esq, No dir) {
-			this.raiz = raiz;
-			this.esq = esq;
-			this.dir = dir;
-		}
-	}*/
-	
-	//Arvore vazia
+	// Arvore vazia
 	public ArvoreAVL() {
 		raiz = null;
 	}
-	
-	//Criar árvore com apenas um nó
+
+	// Criar árvore com apenas um nó
 	public ArvoreAVL(T raiz) {
 		this.raiz = raiz;
 		esq = null;
 		dir = null;
 	}
-	
-	//Criar árvore com nós a esquerda e a direita
+
+	// Criar árvore com nós a esquerda e a direita
 	public ArvoreAVL(T raiz, ArvoreAVL arvEsq, ArvoreAVL arvDir) {
 		this.raiz = raiz;
 		this.esq = arvEsq;
 		this.dir = arvDir;
 	}
-	
-	//Checa se a árvore é vazia
+
+	// Checa se a árvore é vazia
 	public boolean vazia() {
 		return (raiz == null);
 	}
-	
-	//Calcula altura da árvore
+
+	// Calcula altura da árvore
 	public int calculaAltura() {
 		if ((esq == null) && (dir == null))
 			return 0;
@@ -64,17 +57,30 @@ public class ArvoreAVL<T extends Comparable<T>> {
 
 		return 1 + Math.max(altEsq, altDir);
 	}
-	
-	//TODO - Insere nó na árvore
+
+	// TODO - Insere nó na árvore
 	public void insere(ArvoreAVL no) {
-		
-		if(no == null) {
+
+		if (no == null) {
 			throw new IllegalArgumentException("A chave fornecida é null!");
 		}
-		
-	}
-	
-	//TODO balanceamento
-	
 
+	}
+
+	// TODO balanceamento
+
+	// TODO mostrar
+	public void mostra() {
+		
+		System.out.print("(" + this.raiz);
+
+		if (esq != null) {
+			esq.mostra();
+		}
+		if (dir != null) {
+			dir.mostra();
+		}
+
+		System.out.print(")");
+	}
 }
--- MonitoriaEDD2/ArvoreAVL/ArvoreAVL/Main.java
+++ MonitoriaEDD2/ArvoreAVL/ArvoreAVL/Main.java
@@ -6,7 +6,10 @@ public class Main {
 
 	public static void main(String[] args) {
 		ArvoreAVL a1 = new ArvoreAVL(1); 	
+		ArvoreAVL a3 = new ArvoreAVL(3);
+		ArvoreAVL a2 = new ArvoreAVL(2, a1, a3);
 		
+		a2.mostra();
 	}
 
 }
