5f010901c57ea1bbae6efc163b9ee4eb78db9040
Henrique Filho <hbaf71@gmail.com>
2020-04-23 23:24:15 -0300

    inserido classe Nó

--- MonitoriaEDD2/ArvoreAVL/ArvoreAVL/ArvoreAVL.java
+++ MonitoriaEDD2/ArvoreAVL/ArvoreAVL/ArvoreAVL.java
@@ -1,29 +1,39 @@
-/*Feito por Henrique Filho, baseado no material de Estrutura de Dados 1 do professor Pedro Nuno Moura (presente no package "ArvoreBinaria" e "ArvoreBinariaBusca") 
+/*Feito por Henrique Filho, baseado no material de Estrutura de Dados 1 do professor Pedro Nuno Moura 
+(presente no package "ArvoreBinaria" e "ArvoreBinariaBusca") 
  * e no livro "Estrutura de Dados e Seus Algoritmos - 3ª edição" de Jayme Luiz Szwarcfiter e Lilian Markenzon*/
 package ArvoreAVL;
 
-public class ArvoreAVL<T extends Comparable<T>> {
+public class ArvoreAVL<Chave extends Comparable<Chave>, Valor> {
 
-	// Todo nó de uma árvore AVL é uma árvore, contendo uma chave raiz com um valor,
-	// outra árvore a esquerda e outra a direita, que podem ser nulas ou não
-	private T raiz;
-	private ArvoreAVL esq, dir;
+	private No raiz; //raiz da árvore
 
-	/*
-	 * public class No { private T raiz; private No esq, dir;
-	 * 
-	 * public No(T raiz) { this.raiz = raiz; this.esq = null; this.dir = null; }
-	 * 
-	 * public No(T raiz, No esq, No dir) { this.raiz = raiz; this.esq = esq;
-	 * this.dir = dir; } }
-	 */
+	public class No {
+		private Chave chave; // Chave usada nas comparações.
+		private Valor valor; // Informação armazenada.
+		private No esq, dir; // Referências para subárvores esquerda e direita.
+
+		public No(Chave chave, Valor valor) {
+			this.chave = chave;
+			this.valor = valor;
+			this.esq = null;
+			this.dir = null;
+		}
+
+		public No(Chave chave, Valor valor, No esq, No dir)
+		{
+			this.chave = chave;
+			this.valor = valor;
+			this.esq = esq;
+			this.dir = dir;
+		}
+	}
 
 	// Arvore vazia
 	public ArvoreAVL() {
 		raiz = null;
 	}
 
-	// Criar árvore com apenas um nó
+	/* Criar árvore com apenas um nó
 	public ArvoreAVL(T raiz) {
 		this.raiz = raiz;
 		esq = null;
@@ -35,60 +45,61 @@ public class ArvoreAVL<T extends Comparable<T>> {
 		this.raiz = raiz;
 		this.esq = arvEsq;
 		this.dir = arvDir;
-	}
+	}*/
 
 	// Checa se a árvore é vazia
 	public boolean vazia() {
 		return (raiz == null);
 	}
-	
+
 	// Checa se é folha
-	public boolean folha() {
-		if(esq == null && dir == null) {
+	public boolean folha(No no) {
+		if (no.esq == null && no.dir == null) {
 			return true;
 		}
 		return false;
 	}
 
 	// Calcula altura da árvore
-	public int calculaAltura() {
-		if (esq == null && dir == null)
+	public int calculaAltura(No no) {
+		if (no.esq == null && no.dir == null)
 			return 0;
 
 		int altEsq = 0, altDir = 0;
 
-		if (esq != null)
-			altEsq = esq.calculaAltura();
+		if (no.esq != null)
+			altEsq = this.calculaAltura(no.esq);
 
-		if (dir != null)
-			altDir = dir.calculaAltura();
+		if (no.dir != null)
+			altEsq = this.calculaAltura(no.dir);
 
 		return 1 + Math.max(altEsq, altDir);
 	}
+	
 
-	// Mostra árvore
-	public void mostra() {
+	// Mostra árvore a partir do nó
+	public void mostra(No raiz) {
 
 		System.out.print("(" + raiz);
 
-		if (esq != null) {
-			esq.mostra();
+		if (raiz.esq != null) {
+			mostra(raiz.esq);
 		}
-		if (dir != null) {
-			dir.mostra();
+		if (raiz.dir != null) {
+			mostra(raiz.dir);
 		}
 
 		System.out.print(")");
 	}
 
 	// TODO - Insere nó na árvore
-	public void insere(ArvoreAVL no) {
+	public void insere(No no) {
 
+		// Caso o inicial seja nulo
 		if (no == null) {
 			throw new IllegalArgumentException("A chave fornecida é null!");
 		}
-		
-		
+
 	}
 
 	// TODO balanceamento
--- MonitoriaEDD2/ArvoreAVL/ArvoreAVL/Main.java
+++ MonitoriaEDD2/ArvoreAVL/ArvoreAVL/Main.java
@@ -5,9 +5,8 @@ import ArvoreAVL.ArvoreAVL;
 public class Main {
 
 	public static void main(String[] args) {
-		ArvoreAVL a1 = new ArvoreAVL(1); 	
-		ArvoreAVL a3 = new ArvoreAVL(3);
-		ArvoreAVL a2 = new ArvoreAVL(2, a1, a3);
+		ArvoreAVL a1 = new ArvoreAVL();
+		
 		
 		
 	}
