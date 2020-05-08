c402fed2b33059e130e70a2a7c8838701bda75c2
Henrique Filho <hbaf71@gmail.com>
2020-05-08 20:49:23 -0300

    árvores de cada rotação

--- MonitoriaEDD2/ArvoreAVL/ArvoreAVL/Main.java
+++ MonitoriaEDD2/ArvoreAVL/ArvoreAVL/Main.java
@@ -8,13 +8,44 @@ public class Main {
 		ArvoreAVL<Integer> arv = new ArvoreAVL<Integer>();
 		
 		
-		arv.insere(15);
-		arv.insere(8);
-		arv.insere(23);
-		arv.insere(2);
-		arv.insere(12);
+		
+		//Rotação direita
+		/*
+		arv.insere(30);
 		arv.insere(20);
-		arv.insere(18);
+		arv.insere(22);
+		arv.insere(10);
+		arv.insere(13);
+		*/
+		
+		//Rotação esquerda
+		/*
+		arv.insere(10);
+		arv.insere(20);
+		arv.insere(30);
+		arv.insere(13);
+		arv.insere(22);
+		*/
+		
+		//Rotação Dupla Esquerda
+		arv.insere(10);
+		arv.insere(7);
+		arv.insere(30);
+		arv.insere(35);
+		arv.insere(20);
+		arv.insere(11);
+		arv.insere(25);
+		
+		//Rotação Dupla Direita
+		/*
+		arv.insere(30);
+		arv.insere(10);
+		arv.insere(35);
+		arv.insere(7);
+		arv.insere(20);
+		arv.insere(11);
+		arv.insere(25);
+		*/
 		
 		arv.mostra();
 		
