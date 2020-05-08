411de3c9c0e8046f5ea5dd8e1bb937237dcb5beb
Henrique Filho <hbaf71@gmail.com>
2020-05-08 13:58:35 -0300

    arvore teste

--- MonitoriaEDD2/ArvoreAVL/ArvoreAVL/Main.java
+++ MonitoriaEDD2/ArvoreAVL/ArvoreAVL/Main.java
@@ -9,8 +9,12 @@ public class Main {
 		
 		
 		arv.insere(10);
-		arv.insere(20);
+		arv.insere(7);
 		arv.insere(30);
+		arv.insere(35);
+		arv.insere(20);
+		arv.insere(11);
+		arv.insere(25);
 		
 		arv.mostra();
 		
