3c80f5c63fff4f91619532212f6c3890a577a50c
Henrique Filho <hbaf71@gmail.com>
2020-06-13 20:47:21 -0300

    lista e matriz de adjacência prontas

--- MonitoriaEDD2/Grafos/GrafoGenerico/Grafo.java
+++ MonitoriaEDD2/Grafos/GrafoGenerico/Grafo.java
@@ -8,16 +8,49 @@ public class Grafo {
 
 	// Mostrando os vértices por Lista de Adjacência
 	public void mostraLA() {
+		System.out.println("LISTA DE ADJACÊNCIA");
 		// Caso não tenha vértices no grafo
-		if(vertices.size() == 0) {
+		if (vertices.size() == 0) {
 			System.out.println("O grafo não possui vértices");
 			return;
 		}
-		
-		for(int i = 0; i < vertices.size(); i++) {
+
+		for (int i = 0; i < vertices.size(); i++) {
 			vertices.get(i).mostraAdjacentes();
 		}
-		
+		System.out.println();
+	}
+
+	// Mostrando os vértices por Matriz de Adjacência
+	public void mostraMA() {
+		System.out.println("MATRIZ DE ADJACÊNCIA");
+		System.out.print("   ");
+
+		for (int i = 0; i < vertices.size(); i++) {
+			System.out.print(vertices.get(i).chave + " ");
+		}
+		System.out.println();
+
+		for (int i = 0; i < vertices.size(); i++) {
+			System.out.print(vertices.get(i).chave + ": ");
+			for (int j = 0; j < vertices.size(); j++) {
+				// Bolean que controla se o vértice foi encontrado nos adjacentes do vértice i atual
+				boolean encontrou = false;
+				for (int k = 0; k < vertices.get(i).adjacentes.size(); k++) {
+					// Se encontrou, printa 1 e vai para a próxima testagem
+					if (vertices.get(j).chave == vertices.get(i).adjacentes.get(k).chave) {
+						System.out.print("1 ");
+						encontrou = true;
+						break;
+					}
+					// Se chegou na última testagem e não achou, printa 0
+					if ((k == vertices.get(i).adjacentes.size() - 1) && encontrou == false) {
+						System.out.print("0 ");
+					}
+				}
+			}
+			System.out.println();
+		}
 	}
 
 	// Mostra os vértices do grafo
@@ -28,8 +61,9 @@ public class Grafo {
 			System.out.print(vertices.get(i).chave + " ");
 		}
 		System.out.println();
+		System.out.println();
 	}
-	
+
 	// Adiciona o vértice no grafo
 	public void adicionaVertice(Vertice v) {
 		vertices.add(v);
--- MonitoriaEDD2/Grafos/GrafoGenerico/Main.java
+++ MonitoriaEDD2/Grafos/GrafoGenerico/Main.java
@@ -7,22 +7,27 @@ import GrafoGenerico.Grafo;
 public class Main {
 	
 	public static void main(String[] args) {
+		// Criando grafo
 		Grafo g = new Grafo();
 		
+		// Criando vértices
 		Vertice v1 = new Vertice(1);
 		Vertice v2 = new Vertice(2);
 		Vertice v3 = new Vertice(3);
 		
+		// Adicionando vértices no grafo
 		g.adicionaVertice(v1);
 		g.adicionaVertice(v2);
 		g.adicionaVertice(v3);
 		
+		// Ligando vértices
 		v1.liga(v2);
 		v3.liga(v2);
 		v1.liga(v3);
 		
 		g.mostraVertices();
 		g.mostraLA();
+		g.mostraMA();
 	}
 	
 }
