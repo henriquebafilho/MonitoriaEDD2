34ecac146c837b53043d42e08c0c025ee2263862
Henrique Filho <hbaf71@gmail.com>
2020-09-08 14:11:22 -0300

    Tirando as funções da AVL da classe ArvoreBinariaBusca

---
+++ MonitoriaEDD2/ArvoreBinariaBusca/ABB/ArvoreBinariaBusca.java
@@ -0,0 +1,294 @@
+package ABB;
+
+public class ArvoreBinariaBusca<Chave extends Comparable<Chave>> {
+	private No raiz; // raiz da árvore
+
+	private class No {
+		private Chave chave; // Chave usada nas comparações
+		private No esq, dir, paiDesregulado; // Referências para subárvores esquerda e direita
+
+		public No(Chave chave) {
+			this.chave = chave;
+			this.esq = null;
+			this.dir = null;
+		}
+	}
+
+	// Cria árvore com a raiz inicialmente nula
+	public ArvoreBinariaBusca() {
+		raiz = null;
+	}
+
+	// Cria árvore com apenas um nó, que será raiz
+	public ArvoreBinariaBusca(Chave raiz) {
+		No noRaiz = new No(raiz);
+		this.raiz = noRaiz;
+	}
+
+	// Retorna raiz da árvore
+	public No getRaiz() {
+		return this.raiz;
+	}
+
+	// Retorna a chave da raiz
+	public Chave getChaveRaiz() {
+		if (this.raiz == null) {
+			System.out.println("A raiz é nula");
+			return null;
+		}
+		return this.raiz.chave;
+	}
+
+	// Checa se a árvore é vazia
+	public boolean vazia() {
+		return (raiz == null);
+	}
+
+	// Checa se é folha
+	public boolean folha(No no) {
+		if (no == null) {
+			return false;
+		} else if (no.esq == null && no.dir == null) {
+			return true;
+		}
+		return false;
+	}
+
+	// Mostra árvore a partir da raiz
+	public void mostra() {
+		mostra(raiz);
+		System.out.println();
+	}
+
+	private void mostra(No no) {
+		if (no == null) {
+			System.out.println("A árvore é nula");
+			return;
+		}
+
+		System.out.print("(" + no.chave);
+
+		if (no.esq != null) {
+			mostra(no.esq);
+		} else if (!folha(no)) {
+			System.out.print("( )");
+		}
+
+		if (no.dir != null) {
+			mostra(no.dir);
+		} else if (!folha(no)) {
+			System.out.print("( )");
+		}
+		System.out.print(")");
+	}
+
+	// Conta quantos nós tem na árvore a partir da raiz
+	public int contaNos() {
+		return contaNos(raiz);
+	}
+	
+	private int contaNos(No no) {
+		if (no == null) {
+			return 0;
+		}
+
+		if ((raiz.esq == null) && (raiz.dir == null)) {
+			return 1;
+		}
+
+		int nosEsq = 0, nosDir = 0;
+
+		if (no.esq != null) {
+			nosEsq = contaNos(no.esq);
+		}
+		if (no.dir != null) {
+			nosDir = contaNos(no.dir);
+		}
+		return 1 + nosEsq + nosDir;
+	}
+
+	// Procura a chave inserida no método e retorna o nó
+	public No busca(Chave chave) {
+		if (chave == null) {
+			System.out.println("A chave é nula");
+			return null;
+		}
+
+		No procura = busca(raiz, chave);
+
+		if (procura != null) {
+			System.out.println("O nó " + procura.chave + " está na árvore");
+		} else {
+			System.out.println("O nó inserido não está na árvore");
+		}
+		return procura;
+	}
+
+	public No busca(No no, Chave chave) {
+		No inserido = new No(null);
+
+		if (no == null) {
+			System.out.println("O nó inserido é nulo");
+			return null;
+		}
+
+		// Calcula se a chave prcurada é maior, menor ou igual à atual
+		int comparador = chave.compareTo(no.chave);
+
+		// Se for menor, busca na esquerda
+		if (comparador < 0) {
+			inserido = busca(no.esq, chave);
+		}
+		// Se for maior, busca na direita
+		else if (comparador > 0) {
+			inserido = busca(no.dir, chave);
+		}
+		// Encontrou a chave
+		else {
+			inserido = no;
+			return inserido;
+		}
+		return inserido;
+	}
+
+	// Insere nó na árvore, depois balancea a árvore
+	public void insere(Chave chave) {
+		No no = new No(chave);
+
+		// Caso o nó tenha chave nula
+		if (no.chave == null) {
+			throw new IllegalArgumentException("A chave fornecida é null!");
+		}
+		
+		System.out.println("Insere: " + chave);
+		raiz = insere(raiz, chave);
+	}
+
+	private No insere(No no, Chave chave) {
+		// Caso base: encontrou a posição de inserção
+		if (no == null) {
+			return new No(chave);
+		}
+
+		// se a chave inserida for menor que a atual, vai para esquerda
+		if (chave.compareTo(no.chave) < 0) {
+			no.esq = insere(no.esq, chave);
+		}
+		// se a chave inserida for maior que a atual, vai para direita
+		else if (chave.compareTo(no.chave) > 0) {
+			no.dir = insere(no.dir, chave);
+		}
+		// Caso tenha encontrado nó de mesma chave, sobreescreve
+		else {
+			no.chave = chave;
+		}
+		return no;
+	}
+
+	// Pega a menor chave da árvore
+	public Chave min() {
+		if (vazia()) {
+			System.out.println("A árvore está vazia");
+			return null;
+		}
+		return min(raiz).chave;
+	}
+
+	private No min(No atual) {
+		if (atual.esq == null) {
+			return atual;
+		} else {
+			return min(atual.esq);
+		}
+	}
+
+	// Deletar um nó e checa se a árvore está balanceada,
+	public boolean delete(Chave chave) {
+		raiz = delete(raiz, chave);
+
+		if (raiz != null) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	private No delete(No atual, Chave chave) {
+		if (atual == null) {
+			System.out.println("O nó inserido é nulo");
+			return null;
+		}
+
+		// Compara a chave atual com a que deve ser deletada
+		int comparador = chave.compareTo(atual.chave);
+
+		// Se a atual for menor, checa o filho à esquerda
+		if (comparador < 0) {
+			atual.esq = delete(atual.esq, chave);
+		}
+		// Se a atual for maior, checa o filho à direita
+		else if (comparador > 0) {
+			atual.dir = delete(atual.dir, chave);
+		} else {
+			if (atual.dir == null) {
+				return atual.esq;
+			}
+			if (atual.esq == null) {
+				return atual.dir;
+			}
+
+			No aux = atual;
+
+			// Pega o menor da subárvore direita (mais à esquerda)
+			atual = min(aux.dir);
+
+			// Remove o menor nó da sub-árvore direita
+			atual.dir = deleteMin(aux.dir);
+
+			// A subárvore esquerda se mantém a mesma
+			atual.esq = aux.esq;
+		}
+		return atual;
+	}
+
+	// Deleta o menor nó da árvore
+	public void deleteMin() {
+		if (vazia()) {
+			System.out.println("A árvore está vazia");
+			return;
+		}
+		raiz = deleteMin(raiz);
+	}
+
+	private No deleteMin(No atual) {
+		// Caso não tenha filho à esquerda, o nó atual possui a menor chave
+		if (atual.esq == null) {
+			// Se o atual.dir não for nulo, o pai do menor passa a apontar para ele
+			return atual.dir;
+		}
+		atual.esq = deleteMin(atual.esq);
+
+		return atual;
+	}
+
+	// Calcula altura do nó
+	public int calculaAltura(No no) {
+		int altEsq = 0, altDir = 0;
+
+		if (no == null) {
+			return -1;
+		}
+
+		if (folha(no)) {
+			return 0;
+		}
+
+		if (no.esq != null) {
+			altEsq = this.calculaAltura(no.esq);
+		}
+		if (no.dir != null) {
+			altDir = this.calculaAltura(no.dir);
+		}
+		return 1 + Math.max(altEsq, altDir);
+	}
+}
--- MonitoriaEDD2/ArvoreBinariaBusca/ABB/Main.java
+++ MonitoriaEDD2/ArvoreBinariaBusca/ABB/Main.java
@@ -1,87 +1,14 @@
 package ABB;
 
-import ArvoreAVL.ArvoreAVL;
+import ABB.ArvoreBinariaBusca;
 
 public class Main {
 
 	public static void main(String[] args) {
-		
+
 		// Criando uma árvore com o nó 5 como raiz
-		ArvoreAVL<Integer> arv = new ArvoreAVL<Integer>();
-		
+		ArvoreBinariaBusca<Integer> arv = new ArvoreBinariaBusca<Integer>();
 
-		// Árvores Balanceadas
-
-//		arv.insere(8);
-//		arv.insere(9);
-
-		// Rotação direita
-
-//		arv.insere(30);
-//		arv.insere(20);
-//		arv.insere(22);
-//		arv.insere(10);
-//		arv.insere(13);
-
-//		arv.insere(27);
-//		arv.insere(15);
-//		arv.insere(49);
-//		arv.insere(10);
-//		arv.insere(8);
-
-		// Rotação esquerda
-
-//		arv.insere(10);
-//		arv.insere(20);
-//		arv.insere(30);
-//		arv.insere(13);
-//		arv.insere(22);
-
-//		arv.insere(10);
-//		arv.insere(8);
-//		arv.insere(15);
-//		arv.insere(27);
-//		arv.insere(49);
-
-		// Rotação Dupla Esquerda
-
-		arv.insere(10);
-		arv.insere(7);
-		arv.insere(30);
-		arv.insere(35);
-		arv.insere(20);
-		arv.insere(11);
-		arv.insere(25);
-
-		// Rotação Dupla Direita
-
-//		arv.insere(30);
-//		arv.insere(10);
-//		arv.insere(35);
-//		arv.insere(7);
-//		arv.insere(20);
-//		arv.insere(11);
-//		arv.insere(25);
-
-		// Desregulada aleatória
-
-//		arv.insere(5);
-//		arv.insere(6);
-//		arv.insere(7);
-//		arv.insere(8);
-//		arv.insere(9);
-//		arv.insere(10);
-
-//		arv.insere(10);
-//		arv.insere(9);
-//		arv.insere(8);
-//		arv.insere(7);
-//		arv.insere(6);
-//		arv.insere(5);
-
-		// arv.busca(7);
-
-		arv.balancear();
 	}
 
 }
